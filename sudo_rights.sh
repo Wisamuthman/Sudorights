def1dc65770a2d7fc4575200d182e309d0ad1af0
Wisamuthman <132056927+Wisamuthman@users.noreply.github.com>
2023-05-09 16:35:40 +0300

    Update sudo_rights.sh

--- sudo_rights.sh
+++ sudo_rights.sh
@@ -11,7 +11,6 @@
 SUDOERSACCESS=$(cat /etc/sudoers | grep -vE "#|root|%wheel" | grep "ALL=" |awk '{print $1}' |awk -F ':' '{printf "%s ", $1}')
 COUNTSUDOERS=$(cat /etc/sudoers | grep -vE "#|root|%wheel" | grep "ALL=" |awk '{print $1}' |awk -F ':' '{print $1}'|wc -w)
 SUDOERSD=$(cd /etc/sudoers.d && ls -l /etc/sudoers.d | grep -rE "/bin/su -|/usr/bin/su -|ALL=\(ALL\)" |awk '{print $1}' |awk -F ':' '{printf "%s ", $1}')
-
 COUNT=$(cd /etc/sudoers.d && ls -l /etc/sudoers.d | grep -rE "/bin/su -|/usr/bin/su -|ALL=\(ALL\)" |wc -l)
 
 
@@ -20,7 +19,7 @@ COUNT=$(cd /etc/sudoers.d && ls -l /etc/sudoers.d | grep -rE "/bin/su -|/usr/bin
 if [ $COUNTSUDOERS -gt 0 ]
 then
 
-echo "1 \"Sudo Users in sudoers\" - Root rights are granted to $COUNTSUDOERS users : $SUDOERSACCESS "
+echo "1 \"Sudo Users Check\" - Root rights are granted in Sudoers file to $COUNTSUDOERS users : $SUDOERSACCESS "
 
 # if sudoers file has none stay in ok status
 
@@ -29,10 +28,10 @@ else
 # if the sudoers.d directory has greater than 0 file make a warning
     if [ $COUNT -gt 0 ]
     then
-        echo "1 \"Sudo Users in sudoers.d \" - Root rights are granted to $COUNT users : $SUDOERSD"
+        echo "1 \"Sudo Users Check\" - Root rights are granted in sudoers.d directory to $COUNT users : $SUDOERSD"
 # if any sudoers directory or file has less stay in OK status
         else
-                echo "0 \"Sudo Users service \" - Root rights are not granted to anyone"
+                echo "0 \"Sudo Users Check\" - Root rights are not granted to anyone"
     fi
 
 fi
