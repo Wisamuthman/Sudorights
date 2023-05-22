849b25ef2a6b785647ae21692a7fcf183357c273
Wisamuthman <132056927+Wisamuthman@users.noreply.github.com>
2023-05-22 13:13:26 +0300

    Update sudo_rights.sh

--- sudo_rights.sh
+++ sudo_rights.sh
@@ -7,10 +7,10 @@
 # Made by Wisam Uthman
 
 
-SUDOERSACCESS=$(cat /etc/sudoers | grep -vE "#|root|%wheel" | grep -E "ALL=NOPASSWD: /bin/su -|ALL=NOPASSWD: /usr/bin/su -|ALL=NOPASSWD: ALL|ALL=\(ALL\) NOPASSWD: /bin/su -|ALL=\(ALL\) NOPASSWD: /usr/bin/su -|ALL=\(ALL\) NOPASSWD: ALL"|awk '{print $1}' |awk -F ':' '{printf "%s ", $1}')
-COUNTSUDOERS=$(cat /etc/sudoers | grep -vE "#|root|%wheel" | grep -E "ALL=NOPASSWD: /bin/su -|ALL=NOPASSWD: /usr/bin/su -|ALL=NOPASSWD: ALL|ALL=\(ALL\) NOPASSWD: /bin/su -|ALL=\(ALL\) NOPASSWD: /usr/bin/su -|ALL=\(ALL\) NOPASSWD: ALL"|awk '{print $1}' |awk -F ':' '{print $1}'|wc -w)
-SUDOERSD=$(cd /etc/sudoers.d && ls -l /etc/sudoers.d | grep -rE "ALL=\(ALL\) NOPASSWD: /bin/su -($|"root")|ALL=\(ALL\) NOPASSWD: /usr/bin/su -($|"root")|ALL=\(ALL\) NOPASSWD: ALL($|\s|"root")"|awk '{print $1}' |awk -F ':' '{printf "%s ", $1}')
-COUNT=$(cd /etc/sudoers.d && ls -l /etc/sudoers.d | grep -rE "ALL=\(ALL\) NOPASSWD: /bin/su -($|"root")|ALL=\(ALL\) NOPASSWD: /usr/bin/su -($|"root")|ALL=\(ALL\) NOPASSWD: ALL($|\s|"root")"|wc -l)
+SUDOERSACCESS=$(cat /etc/sudoers | grep -vE "#|root|%wheel" | grep -E "ALL=NOPASSWD: /bin/su -[[:space:]]|ALL=NOPASSWD: /usr/bin/su -[[:space:]]|ALL=NOPASSWD: ALL|ALL=\(ALL\) NOPASSWD: /bin/su -[[:space:]]|ALL=\(ALL\) NOPASSWD: /usr/bin/su -[[:space:]]|ALL=\(ALL\) NOPASSWD: ALL[[:space:]]"|awk '{print $1}' |awk -F ':' '{printf "%s ", $1}')
+COUNTSUDOERS=$(cat /etc/sudoers | grep -vE "#|root|%wheel" | grep -E "ALL=NOPASSWD: /bin/su -[[:space:]]|ALL=NOPASSWD: /usr/bin/su -[[:space:]]|ALL=NOPASSWD: ALL|ALL=\(ALL\) NOPASSWD: /bin/su -[[:space:]]|ALL=\(ALL\) NOPASSWD: /usr/bin/su -[[:space:]]|ALL=\(ALL\) NOPASSWD: ALL[[:space:]]"|awk '{print $1}' |awk -F ':' '{print $1}'|wc -w)
+SUDOERSD=$(cd /etc/sudoers.d && ls -l /etc/sudoers.d | grep -rE "ALL=\(ALL\) NOPASSWD: /bin/su -[[:space:]]*($|"root")|ALL=\(ALL\) NOPASSWD: /usr/bin/su -[[:space:]]*($|"root")|ALL=\(ALL\) NOPASSWD: ALL[[:space:]]*($|\s|"root")"|awk '{print $1}' |awk -F ':' '{printf "%s ", $1}')
+COUNT=$(cd /etc/sudoers.d && ls -l /etc/sudoers.d | grep -rE "ALL=\(ALL\) NOPASSWD: /bin/su -[[:space:]]*($|"root")|ALL=\(ALL\) NOPASSWD: /usr/bin/su -[[:space:]]*($|"root")|ALL=\(ALL\) NOPASSWD: ALL[[:space:]]*($|\s|"root")"|wc -l)
 
 
 #if sudoers file has more than 0 root account make a warning alert
